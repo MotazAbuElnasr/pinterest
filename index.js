8bc39a73fb4c0da317ced763614edfb4a46dc7a7
MotazAbuElnasr <motaz.abuelnasr@gmail.com>
2019-06-21 15:50:25 +0200

    change server static path

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
-app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
 app.use(cors());
 app.use("/posts", postRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log(`Server Started at port ${PORT}`);
 });
