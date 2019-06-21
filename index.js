935b9308710920cdeaa6bcea9817bcd32d7a07a8
MotazAbuElnasr <motaz.abuelnasr@gmail.com>
2019-06-21 15:31:29 +0200

    add express.static to fix deply

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
-
+app.use(express.static(root));
 app.use(express.json());
 app.use(cors());
 app.use("/posts", postRouter);
